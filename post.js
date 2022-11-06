d0ed447eb062b47ace93e2903a4f96d67f0fca17
Duck <87908967+bcomey1@users.noreply.github.com>
2022-11-05 19:46:05 -0700

    Upvotes

--- post.js
+++ post.js
@@ -1,2 +1,8 @@
-document.getElementById("title").innerHTML = localStorage.getItem("title");
-document.getElementById("content").innerHTML = localStorage.getItem("content");
+let postID = JSON.parse(localStorage.getItem("currPostID"));
+let post = JSON.parse(localStorage.getItem("Posts"))[postID];
+document.getElementById("title").innerHTML = post.title;
+document.getElementById("content").innerHTML = post.content;
+
+function upvote() {
+    post.upvotes++;
+}
--- questions.js
+++ questions.js
@@ -4,11 +4,11 @@ function createConversation()
   opened.document.write('<html><head><title>Create a Post</title><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"><link href="style.css" rel="stylesheet" type="text/css" /><link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Raleway&display=swap" </head><body><div id="TitleText"> Question </div><input type="text" id="title"</input><button class="b" id="submit" onclick="getInput()">Submit</button><script src="script.js"></script></html>');
 } 
 
-function viewPost(title, content)
+function viewPost(postID)
 {
- // alert(content)
- localStorage.setItem("title", title);
- localStorage.setItem("content", content);
+  post = JSON.parse(localStorage.getItem("Posts"))[postID];
+ localStorage.setItem("currPostID", postID);
+
  
  window.location.href = "post.html";
  //Put post information in html 
@@ -21,9 +21,8 @@ for(var i=0;i<post_list.length;i++){
   var div = document.createElement('div');
   var button = document.createElement('button');
   button.className = 'exists review';
-  button.setAttribute("title", post_list[i].title);
-  button.setAttribute("id", post_list[i].content);
-  button.setAttribute("onclick", "viewPost(this.title, this.id)")
+  button.setAttribute("id", i);
+  button.setAttribute("onclick", "viewPost(this.id)");
   div.textContent = post_list[i].content;
   div.className = "reviewDesc";
   button.appendChild(div);
